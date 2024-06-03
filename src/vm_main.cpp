b88a3137c7330aff8b24481dfc63b00e72032129
cubejun <133946040+cubejun@users.noreply.github.com>
2024-06-03 20:41:26 +0900

    Delete src directory

--- src/vm_main.cpp
+++
@@ -1,9 +0,0 @@
-#include "line_tracer/vm.hpp"
-int main(int argc, char * argv[])
-{
-    rclcpp::init(argc, argv);
-    auto node = std::make_shared<Sub>();
-    rclcpp::spin(node);
-    rclcpp::shutdown();
-    return 0;
-}
